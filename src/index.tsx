aeb88fdf09550260e0fc02927ff5243b8268a8e7
barretojs <barreto.carvalho.b@gmail.com>
2022-04-29 18:45:04 -0300

    fixing annoying createRoot warning

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import globalStore from "@store/globalStore";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+root.render(
   <React.StrictMode>
     <Provider store={globalStore}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
